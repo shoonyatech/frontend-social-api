ac000890fc5b11ee34a4c77f7a52decc4c36c98b
Sanjay Singh <ssingh@falabella.cl>
2019-03-25 01:32:28 +0530

    Initial Commit

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,8 +1,8 @@
-import { Router } from 'express';
-import sample from "./sample";
+import { Router } from "express";
+import users from "./users";
 
 const router = Router();
-
-router.use("/sample", sample);
+router.get("/", (req, res) => res.send("Welcome to Frontend Social API"));
+router.use("/users", users);
 
 export default router;
---
+++ src/mockDb/users.js
@@ -0,0 +1,237 @@
+export const users = [
+  {
+    name: {
+      title: "Mr",
+      first: "Sanjay",
+      last: "Singh"
+    },
+    email: "singhsanjayrd@gmail.com",
+    photo: "someLink.com",
+    socialLinks: {
+      github: "github.com",
+      twitter: "twitter.com",
+      linkedin: "linkedin.com",
+      bitbucket: "bitbucket.com",
+      medium: "medium.com",
+      website: "sample.com"
+    },
+    technicalSkills: [
+      {
+        name: "React",
+        rating: "9"
+      },
+      {
+        name: "Angular",
+        rating: "8"
+      },
+      {
+        name: "Vue",
+        rating: "8"
+      },
+      {
+        name: "Ember",
+        rating: "8"
+      },
+      {
+        name: "Web Components",
+        rating: "8"
+      },
+      {
+        name: "JQuery",
+        rating: "8"
+      },
+      {
+        name: "Backbone",
+        rating: "8"
+      }
+    ],
+    conferences: [
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      }
+    ],
+    meetups: [
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      },
+      {
+        id: "",
+        title: "jsFoo",
+        startDate: "",
+        endDate: ""
+      }
+    ]
+  }
+];
+
+export const user = {
+  name: {
+    title: "Mr",
+    first: "Sanjay",
+    last: "Singh"
+  },
+  email: "singhsanjayrd@gmail.com",
+  photo: "someLink.com",
+  socialLinks: {
+    github: "github.com",
+    twitter: "twitter.com",
+    linkedin: "linkedin.com",
+    bitbucket: "bitbucket.com",
+    medium: "medium.com",
+    website: "sample.com"
+  },
+  technicalSkills: [
+    {
+      name: "React",
+      rating: "9"
+    },
+    {
+      name: "Angular",
+      rating: "8"
+    },
+    {
+      name: "Vue",
+      rating: "8"
+    },
+    {
+      name: "Ember",
+      rating: "8"
+    },
+    {
+      name: "Web Components",
+      rating: "8"
+    },
+    {
+      name: "JQuery",
+      rating: "8"
+    },
+    {
+      name: "Backbone",
+      rating: "8"
+    }
+  ],
+  conferences: [
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    }
+  ],
+  meetups: [
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    },
+    {
+      id: "",
+      title: "jsFoo",
+      startDate: "",
+      endDate: ""
+    }
+  ]
+};
