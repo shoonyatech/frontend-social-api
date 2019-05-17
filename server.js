dbb7df357d11dc9cc146bbc307b09b77d78945fd
Souvik Basu <souvikbasu@gmail.com>
2019-05-17 08:20:26 +0530

    add city and conference routes to server.js

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get("/", function(req, res) {
 
 require("./routes/auth.routes.js")(app);
 require("./routes/job.routes.js")(app);
+require("./routes/city.routes.js")(app);
+require("./routes/conference.routes.js")(app);
 
 // this will attach the logged in user to req.user
 const JWT_SECRET = process.env.JWT_SECRET || "verySecret$%#$%@#!#!$!!";
