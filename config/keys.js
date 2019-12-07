87a60ecd5e43661f09fb0e71ee149db475aa3d9c
Souvik Basu <souvikbasu@gmail.com>
2019-12-07 00:57:23 -0800

    add empty keys file

---
+++ config/keys.js
@@ -0,0 +1,24 @@
+exports.keys = {
+  mongodb: "",
+  jwtSecret: "",
+  facebook: {
+    clientId: "",
+    clientSecret: ""
+  },
+  github: {
+    clientId: "",
+    clientSecret: ""
+  },
+  twitter: {
+    clientId: "",
+    clientSecret: ""
+  },
+  bitbucket: {
+    clientId: "",
+    clientSecret: ""
+  },
+  linkedin: {
+    clientId: "",
+    clientSecret: ""
+  }
+};
