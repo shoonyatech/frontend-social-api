594a365533a52c0ece5780a74d84fca2fa31663b
Souvik Basu <souvikbasu@gmail.com>
2019-12-17 22:40:59 -0800

    return user profile for a given username

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,15 +4,18 @@ module.exports = app => {
   // retrieve current user profile
   app.get("/me", profile.me);
 
-  // Retrieve all profile
-  // app.get("/profile", profile.findAll);
-
-  // Retrieve a single profile with id
-  // app.get("/profile/:id", profile.findOne);
-
   // Update my profile
   app.put("/me", profile.update);
 
   // Delete my profile
   app.delete("/me", profile.delete);
+
+  // retrieve current user profile
+  app.get("/user/:username", profile.findByUsername);
+
+  // Retrieve all profile
+  // app.get("/profile", profile.findAll);
+
+  // Retrieve a single profile with id
+  // app.get("/profile/:id", profile.findOne);
 };
