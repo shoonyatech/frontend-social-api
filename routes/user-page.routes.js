fe561408c2d99ddb14bcfc3f5d091ae1019258d1
Souvik Basu <souvikbasu@gmail.com>
2020-04-28 00:05:34 -0700

    change comment for user page count call

--- routes/user-page.routes.js
+++ routes/user-page.routes.js
@@ -1,15 +1,15 @@
 module.exports = (app) => {
-    const userPage = require("../controllers/user-page.controller.js");
+  const userPage = require("../controllers/user-page.controller.js");
 
-    // Create a new userpage data
-    app.post("/userpage", userPage.create);
+  // Create a new userpage data
+  app.post("/userpage", userPage.create);
 
-    // Delete a userpage
-    app.delete("/userpage", userPage.delete);
+  // Delete a userpage
+  app.delete("/userpage", userPage.delete);
 
-    // get all online user by page
-    app.post("/userpage/online/", userPage.findAllUserByURL);
+  // get all online user by page
+  app.post("/userpage/online/", userPage.findAllUserByURL);
 
-    // Create a new userpage data
-    app.post("/userpage/online/count", userPage.getUserCountByURL);
+  // get count of online users
+  app.post("/userpage/online/count", userPage.getUserCountByURL);
 };
