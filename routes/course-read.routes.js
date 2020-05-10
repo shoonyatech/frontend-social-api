52168789959de742d6a03b083f915e8ec3b438ca
Srashti Jain <srashti0204@gmail.com>
2020-05-11 03:57:41 +0530

    Added route findOneByTopicId

--- routes/course-read.routes.js
+++ routes/course-read.routes.js
@@ -1,10 +1,13 @@
 module.exports = app => {
-    const course = require("../controllers/course.controller.js");
-  
-    // Retrieve all course
-    app.get("/course", course.findAll);
-  
-    // Retrieve a single course with id
-    app.get("/course/:id", course.findOne);
-  };
-  
+  const course = require("../controllers/course.controller.js");
+
+  // Retrieve all course
+  app.get("/course", course.findAll);
+
+  // Retrieve a single course with id
+  app.get("/course/:id", course.findOne);
+
+  // Retrieve a single topic with id
+  app.get("/course/topic/:id", course.findOneByTopicId);
+
+};
