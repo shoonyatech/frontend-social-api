08d64e713238565d6f20646c31ae997c4647bc41
Mayank Jain <mayank.jain@factset.com>
2020-06-25 00:44:50 +0530

    Feat(Event) Fix events order

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -45,7 +45,7 @@ exports.findAll = (req, res) => {
   const page = Number(req.query.page) || 1
 
   CityEvent.find(finalQuery)
-    .sort({ dateFrom: "ascending" })
+    .sort({ dateFrom: "descending" })
     .limit(limit)
     .skip(limit * (page - 1))
     .then((events) => {
