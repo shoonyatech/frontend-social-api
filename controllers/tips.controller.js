4653bcb725a544fa76d86cf10b4ee319edc6f97f
Mayank Jain <mayank.jain@factset.com>
2020-07-04 00:32:05 +0530

    Feat(Tips) Filters for tool-tip

--- controllers/tips.controller.js
+++ controllers/tips.controller.js
@@ -12,12 +12,45 @@ exports.create = async (req, res) => {
 }
 
 exports.findAll = async (req, res) => {
+  const andQuery = getQuery(req.query);
+
+  let finalQuery = {};
+  if (andQuery.length) {
+    finalQuery = { $and: andQuery };
+  }
   try {
-   const tips = await Tip.find({});
+   const tips = await Tip.find(finalQuery);
    res.send(tips);
   } catch (err) {
     res.status(500).send(err || 'error occurred while getting tips');
   }
+}
+
+function getQuery(query) {
+  const tags = query.tags;
+  let tagsQuery = {};
+  let andQuery = [];
+
+  if (tags) {
+    let _tags = tags.split(",");
+    if (_tags.length) {
+      tagsQuery["$or"] = [{ tags: { $in: _tags } }];
+      andQuery.push(tagsQuery);
+    }
+  }
+  return andQuery;
+}
+
+exports.getAllTags = async (req, res) => {
+  try {
+    const tips = await Tip.find({});
+    let tags = tips.reduce((acc, val) => {
+     return acc.concat(val.tags);
+    }, []);
+    res.send(Array.from(new Set(tags)));
+   } catch (err) {
+     res.status(500).send(err || 'error occurred while getting tags');
+   }
 }
 
 exports.findById = async (req, res) => {
