934c595e6869673b07c7a71fbf5d75578df92773
Souvik Basu <souvikbasu@gmail.com>
2020-09-12 22:11:36 -0700

    feat(challenge): fetch challenge by uniqueId

--- routes/challenge-read.routes.js
+++ routes/challenge-read.routes.js
@@ -6,4 +6,6 @@ module.exports = app => {
 
   // Retrieve a single challenge with id
   app.get("/challenge/:id", challenge.findById);
+
+  app.get("/challenge/byUniqueId/:id", challenge.findByUniqueId);
 };
