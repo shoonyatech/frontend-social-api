ca02a5f3552215a20fbbee2bef2b94f7571d9199
Souvik Basu <souvikbasu@gmail.com>
2020-09-18 23:50:08 -0700

    feat(quiz): add methods to read quiz runs

--- controllers/quizRun.controller.js
+++ controllers/quizRun.controller.js
@@ -1,7 +1,7 @@
 const QuizRun = require("../models/quizRun.model.js");
 
 exports.create = (req, res) => {
-    const uniqueId = Math.floor(Math.random() * 10000) + 1;
+  const uniqueId = Math.floor(Math.random() * 10000) + 1;
   const quizRun = new QuizRun({ ...req.body, createdBy: req.user , uniqueId });
   quizRun
     .save()
@@ -12,6 +12,46 @@ exports.create = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the quizRun.",
+      });
+    });
+};
+
+// Retrieve and return all quizs from the database.
+exports.findAll = async (req, res) => {
+  QuizRun.find()
+    .then((quizRuns) => {
+      let response = { results: [] };
+      if (quizRuns.length) {
+        response.results = quizRuns;
+      }
+      res.send(response);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving quizes.",
+      });
+    });
+};
+
+// Find a single quiz with a id
+exports.findOne = (req, res) => {
+  QuizRun.find({ uniqueId: req.params.uniqueId})
+    .then((quizRun) => {
+      if (!quizRun) {
+        return res.status(404).send({
+          message: "quiz not found with uniqueId " + req.params.uniqueId,
+        });
+      }
+      res.send(quizRun);
+    })
+    .catch((err) => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "quiz not found with uniqueId " + req.params.uniqueId,
+        });
+      }
+      return res.status(500).send({
+        message: "Error retrieving quiz with uniqueId " + req.params.uniqueId,
       });
     });
 };
