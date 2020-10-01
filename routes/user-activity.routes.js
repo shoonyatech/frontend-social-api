6826fb9abf5e0398d50b47401fe8094cfd5e99be
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-10-01 21:43:01 +0530

    feat(profile): add profile username to find user Engagement by username

--- routes/user-activity.routes.js
+++ routes/user-activity.routes.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
-    const userActivity = require("../controllers/user-activity.controller.js");
+  const userActivity = require("../controllers/user-activity.controller.js");
 
-    // Get User Activity
-    app.get("/useractivity", userActivity.findAllByUser);
+  // Get User Activity
+  app.get("/useractivity/:username", userActivity.findAllByUser);
 
-    // Create a new useractivity
-    app.post("/useractivity", userActivity.create);
+  // Create a new useractivity
+  app.post("/useractivity", userActivity.create);
 };
