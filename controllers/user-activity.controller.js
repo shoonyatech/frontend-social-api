735d921a202675689b8e8a12eea6db77cdb0d82e
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-10-01 21:43:01 +0530

    feat(profile): add profile username to find user Engagement by username

--- controllers/user-activity.controller.js
+++ controllers/user-activity.controller.js
@@ -2,32 +2,36 @@ const UserActivity = require("../models/user-activity.model.js");
 
 // Create and Save a new user-activiy
 exports.create = (req, res) => {
-    const userActivity = new UserActivity({ ...req.body, username: req.user.username, createdBy: req.user });
+  const userActivity = new UserActivity({
+    ...req.body,
+    username: req.user.username,
+    createdBy: req.user,
+  });
 
-    // Save user-activiy in the database
-    userActivity
-        .save()
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the user-activiy.",
-            });
-        });
+  // Save user-activiy in the database
+  userActivity
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the user-activiy.",
+      });
+    });
 };
 
 // Retrieve and return all  user-activiy from the database by user.
 exports.findAllByUser = (req, res) => {
-    UserActivity.find({ username: req.user.username })
-        .then((userActivities) => {
-            res.send(userActivities);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving comments.",
-            });
-        });
+  UserActivity.find({ username: req.params.username })
+    .then((userActivities) => {
+      res.send(userActivities);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving comments.",
+      });
+    });
 };
-
