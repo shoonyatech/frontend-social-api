1851b94e09a69be2937854f877ed4ab7f95bea66
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-10-03 17:02:25 +0530

    feat(city) : add null check to add city

--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -1,22 +1,34 @@
+const { Logger } = require("mongodb");
 const City = require("../models/city.model.js");
 
 // Create and Save a new city
 exports.create = async (req, res) => {
   const city = await this.createCityIfNotExists(req.body);
+  if (city == null) {
+    return res.status(404).send({
+      message: `city or county code not available`,
+    });
+  }
   res.send(city);
 };
 
-exports.createCityIfNotExists = async cityDetails => {
-  if (cityDetails.name == null || cityDetails.country == null) {
+exports.createCityIfNotExists = async (cityDetails) => {
+  if (
+    cityDetails.name == null ||
+    cityDetails.country == null ||
+    cityDetails.name === "" ||
+    cityDetails.country === ""
+  ) {
     return null;
   }
 
   const existingCity = await City.find({
     name: cityDetails.name,
-    country: cityDetails.country
+    country: cityDetails.country,
   });
 
   if (existingCity.length) {
+    console.log(existingCity);
     return existingCity[0];
   }
 
@@ -34,7 +46,7 @@ exports.findAll = (req, res) => {
   if (citySearchText && citySearchText.length) {
     cityName["$or"] = [
       { name: { $regex: citySearchText, $options: "i" } },
-      { oldName: { $regex: citySearchText, $options: "i" } }
+      { oldName: { $regex: citySearchText, $options: "i" } },
     ];
     orQuery.push(cityName);
   }
@@ -48,19 +60,19 @@ exports.findAll = (req, res) => {
     finalQuery = { $or: orQuery };
   }
 
-  const limit = Number(req.query.limit) || 100
-  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 100;
+  const page = Number(req.query.page) || 1;
 
   City.find(finalQuery)
     .sort({ name: "ascending" })
     .limit(limit)
     .skip(limit * (page - 1))
-    .then(cities => {
+    .then((cities) => {
       res.send(cities);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving citys."
+        message: err.message || "Some error occurred while retrieving citys.",
       });
     });
 };
@@ -70,22 +82,22 @@ exports.findOne = (req, res) => {
   const cityName = req.params.cityName;
   const countryCode = req.params.countryCode;
   City.findOne({ name: cityName, country: countryCode })
-    .then(city => {
+    .then((city) => {
       if (!city) {
         return res.status(404).send({
-          message: `city not found with name ${cityName} and county code ${countryCode}`
+          message: `city not found with name ${cityName} and county code ${countryCode}`,
         });
       }
       res.send(city);
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.kind === "ObjectId") {
         return res.status(404).send({
-          message: "city not found with id " + req.params.id
+          message: "city not found with id " + req.params.id,
         });
       }
       return res.status(500).send({
-        message: "Error retrieving city with id " + req.params.id
+        message: "Error retrieving city with id " + req.params.id,
       });
     });
 };
@@ -95,26 +107,26 @@ exports.update = (req, res) => {
   City.findByIdAndUpdate(
     req.params.id,
     {
-      ...req.body
+      ...req.body,
     },
     { new: true }
   )
-    .then(city => {
+    .then((city) => {
       if (!city) {
         return res.status(404).send({
-          message: "city not found with id " + req.params.id
+          message: "city not found with id " + req.params.id,
         });
       }
       res.send(city);
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.kind === "ObjectId") {
         return res.status(404).send({
-          message: "city not found with id " + req.params.id
+          message: "city not found with id " + req.params.id,
         });
       }
       return res.status(500).send({
-        message: "Error updating city with id " + req.params.id
+        message: "Error updating city with id " + req.params.id,
       });
     });
 };
@@ -122,22 +134,22 @@ exports.update = (req, res) => {
 // Delete a city with the specified id in the request
 exports.delete = (req, res) => {
   City.findByIdAndRemove(req.params.id)
-    .then(city => {
+    .then((city) => {
       if (!city) {
         return res.status(404).send({
-          message: "city not found with id " + req.params.id
+          message: "city not found with id " + req.params.id,
         });
       }
       res.send({ message: "city deleted successfully!" });
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.kind === "ObjectId" || err.name === "NotFound") {
         return res.status(404).send({
-          message: "city not found with id " + req.params.id
+          message: "city not found with id " + req.params.id,
         });
       }
       return res.status(500).send({
-        message: "Could not delete city with id " + req.params.id
+        message: "Could not delete city with id " + req.params.id,
       });
     });
 };
