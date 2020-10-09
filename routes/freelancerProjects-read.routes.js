803d3e4a59d39bc71a937db685a9aecc94b324cb
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-10-10 03:09:27 +0530

    feat(freelancingProject): add update delete get api for freelancer projects

---
+++ routes/freelancerProjects-read.routes.js
@@ -0,0 +1,21 @@
+module.exports = (app) => {
+  const freelanceProjects = require("../controllers/freelancerProjects.controller.js");
+
+  // Retrieve all freelance
+  app.get("/freelance-project", freelanceProjects.findAll);
+
+  // Retrieve a single freelance with id
+  app.get("/freelance-project/:id", freelanceProjects.findOne);
+
+  app.get("/freelances-project/skills", freelanceProjects.getAllSkills);
+  app.get("/freelances-project/jobType", freelanceProjects.getAllJobType);
+  app.get(
+    "/freelances-project/budgetBasis",
+    freelanceProjects.getAllBudgetBasis
+  );
+  app.get("/freelances-project/budget", freelanceProjects.getAllBudget);
+  app.get(
+    "/freelances-project/workDuration",
+    freelanceProjects.getAllWorkDuration
+  );
+};
